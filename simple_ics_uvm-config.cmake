fcff5342d2564489e29700ef3dd8c44fb83b59b1
Akira Nishiyama <mr081677@gmail.com>
2020-10-03 23:16:01 +0900

    improve config.cmake

--- simple_ics_uvm-config.cmake
+++ simple_ics_uvm-config.cmake
@@ -8,6 +8,11 @@
 
 # Compute paths
 find_package(simple_uart_uvm REQUIRED)
+if(CMAKE_VERSION VERSION_GREATER 3.10)
+    if (NOT ${PACKAGE_NAME}_FOUND)
+        require(simple_uart_uvm)
+    endif()
+endif()
 set(simple_ics_uvm_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
 set(simple_ics_uvm_SRC_FILES "") #empty. this package is for simulation only.
 file(GLOB simple_ics_uvm_SIM_FILES ${CMAKE_CURRENT_LIST_DIR}/src/*.sv)
